d31e1d5cc6ba62f2f836f1281039f6db905d03b8
DARYA SHCHEGLOVA <DaryaSHCH@github.com>
2022-10-31 22:25:00 +0400

    add app/src/main/java/hexlet/code/StaticVariables.java

---
+++ app/src/main/java/hexlet/code/StaticVariables.java
@@ -0,0 +1,16 @@
+package hexlet.code;
+
+public class StaticVariables {
+    final static int USER_CHOICE_FOR_GREETING = 1;
+    final static int USER_CHOICE_FOR_START_EVEN_GAME = 2;
+    final static int USER_CHOICE_FOR_EXIT = 0;
+
+    final static int USER_CHOICE_FOR_CALCULATOR = 3;
+    final static int USER_CHOICE_FOR_GCD = 4;
+    final static int USER_CHOICE_FOR_PROGRESSION = 5;
+    final static int USER_CHOICE_FOR_PRIME = 6;
+
+    public final static String QUESTION = "Question: ";
+
+    public final static int TRIES = 3;
+}
