0c7c84dd0cae16f0aead3cbaa2c9ceea2cd45927
DARYA SHCHEGLOVA <DaryaSHCH@github.com>
2022-11-01 17:59:15 +0400

    update app/src/main/java/hexlet/code/Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,21 +1,22 @@
 package hexlet.code;
 
-import java.util.Scanner;
+import static hexlet.code.Engine.SCAN;
 
 public class Cli {
     public static void greetUser() {
-        Scanner scan = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!");
-
         String userName;
-        do {
-            System.out.println("May I have your name?");
-            userName = scan.nextLine();
-        } while (userName.trim().isBlank());
-
+        userName = getUserName("May I have your name?");
         System.out.println("Hello, " + userName + "!");
-
         App.USER_NAME = userName;
-        scan.close();
+    }
+    public static String getUserName(String message){
+        String userName;
+        userName = SCAN.nextLine();
+        while (userName.trim().equals("")) {
+                System.out.println(message);
+            userName = SCAN.nextLine();
+            }
+        return userName;
     }
 }
