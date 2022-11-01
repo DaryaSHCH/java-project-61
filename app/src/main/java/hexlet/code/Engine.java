4f57fbb48ebd288e26cba3ef5f1e642db9a5056e
DARYA SHCHEGLOVA <DaryaSHCH@github.com>
2022-11-01 12:59:37 +0400

    update app/src/main/java/hexlet/code/Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -5,27 +5,40 @@ import java.util.Scanner;
 
 public class Engine {
     static int inputChoiceNumber() {
-        Scanner scan = new Scanner(System.in);
-        int number;
-        while (!scan.hasNextInt()){
-            var line = scan.nextLine();
-            System.out.println(App.USER_CHOICE);
+            Scanner scan = new Scanner(System.in);
+            int number;
+            while (!scan.hasNextInt()){
+                var line = scan.nextLine();
+                System.out.println(App.USER_CHOICE);
+            }
+            number = scan.nextInt();
+            return number;
         }
-        number = scan.nextInt();
-        return number;
-    }
-    static String getUserStringAnswer(String message) {
-        Scanner scan = new Scanner(System.in);
-        String userAnswer;
-        do {
-            System.out.println(message);
+        public static String getUserStringAnswer(String message) {
+            Scanner scan = new Scanner(System.in);
+            String userAnswer;
+            do {
+                System.out.println(message);
 
-            userAnswer = scan.nextLine();
-        } while (userAnswer.trim().equals(""));
+                userAnswer = scan.nextLine();
+            } while (userAnswer.trim().equals(""));
 
-        return userAnswer;
-    }
-    static int getUserIntegerAnswer(String message) {
+            return userAnswer;
+        }
+        public static int getRandomNumberToTen() {
+            int min = 1;
+            int max = 10;
+            Random random = new Random();
+            return random.nextInt(max - min) + min;
+        }
+        public static int getRandomNumberToHundred(){
+            int min = 1;
+            int max = 100;
+            Random random = new Random();
+            return random.nextInt(max - min) + min;
+        }
+    public static int getUserIntegerAnswer(String message) {
+
         Scanner scan = new Scanner(System.in);
         int userAnswer;
         while (!scan.hasNextInt()){
@@ -33,18 +46,6 @@ public class Engine {
             System.out.println(message);
         }
         userAnswer = scan.nextInt();
-        return userAnswer ;
-    }
-    public static int getRandomNumberToTen() {
-        int min = 1;
-        int max = 10;
-        Random random = new Random();
-        return random.nextInt(max - min) + min;
-    }
-    public static int getRandomNumberToHundred(){
-        int min = 1;
-        int max = 100;
-        Random random = new Random();
-        return random.nextInt(max - min) + min;
+        return userAnswer;
     }
 }
