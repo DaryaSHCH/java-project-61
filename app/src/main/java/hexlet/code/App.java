c8e1f19c274783ac468556b9f35898d65c4f09dd
DARYA SHCHEGLOVA <DaryaSHCH@github.com>
2022-11-01 12:59:13 +0400

    update app/src/main/java/hexlet/code/App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,2 +1,58 @@
-package hexlet.code;public class App {
+package hexlet.code;
+
+import hexlet.code.games.*;
+
+import java.util.Scanner;
+
+import static hexlet.code.Cli.greetUser;
+
+public class App {
+    public static String USER_NAME;
+    static String USER_CHOICE = """
+            Please enter the game number and press Enter\s
+            1 - Greet
+            2 - Even\s
+            3 - Calc\s
+            4 - GCD\s
+            5 - Progression\s
+            6 - Prime\s
+            0 - Exit""";
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println(App.USER_CHOICE);
+
+        int numberUserChoice;
+        do {
+            numberUserChoice = Engine.inputChoiceNumber();
+        } while (numberUserChoice > 6);
+
+        if(numberUserChoice == StaticVariables.USER_CHOICE_FOR_GREETING) {
+            greetUser();
+        } else if(numberUserChoice == StaticVariables.USER_CHOICE_FOR_START_EVEN_GAME) {
+            System.out.println("Your choice: " + StaticVariables.USER_CHOICE_FOR_START_EVEN_GAME);
+            greetUser();
+            EvenGame.start();
+        } else if (numberUserChoice == StaticVariables.USER_CHOICE_FOR_CALCULATOR){
+            System.out.println("Your choice: " + StaticVariables.USER_CHOICE_FOR_CALCULATOR);
+            greetUser();
+            CalculatorGame.start();
+        } else if (numberUserChoice == StaticVariables.USER_CHOICE_FOR_GCD) {
+            System.out.println("Your choice: " + StaticVariables.USER_CHOICE_FOR_GCD);
+            greetUser();
+            GreatestCommonDivisorGame.start();
+        } else if (numberUserChoice == StaticVariables.USER_CHOICE_FOR_PROGRESSION) {
+            System.out.println("Your choice: " + StaticVariables.USER_CHOICE_FOR_PROGRESSION);
+            greetUser();
+            ArithmeticProgression.start();
+        } else if (numberUserChoice == StaticVariables.USER_CHOICE_FOR_PRIME) {
+            System.out.println("Your choice: " + StaticVariables.USER_CHOICE_FOR_PRIME);
+            greetUser();
+            PrimeNumberGame.start();
+        } else {
+            System.out.println("Your choice: " + StaticVariables.USER_CHOICE_FOR_EXIT + "\nGoodbye");
+        }
+    }
 }
