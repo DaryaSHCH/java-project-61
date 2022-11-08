a70b6d4ad010b7d3de5c0a94c018d062080465ef
DARYA SHCHEGLOVA <DaryaSHCH@github.com>
2022-11-08 18:19:16 +0400

    update EvenGame

--- app/src/main/java/hexlet/code/games/EvenGame.java
+++ app/src/main/java/hexlet/code/games/EvenGame.java
@@ -4,7 +4,10 @@ import hexlet.code.App;
 import hexlet.code.Engine;
 import hexlet.code.StaticVariables;
 
+import static hexlet.code.Engine.SCAN;
+
 public class EvenGame {
+
     public static void start() {
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
         getEvenGame();
@@ -13,20 +16,14 @@ public class EvenGame {
     public static void getEvenGame() {
         int counterCorrectAnswer = 0;
         String correctAnswer;
-        for (int i = 0; i < StaticVariables.TRIES; i++) {
+        String userAnswer;
+        int tries = 0;
+        do {
+            tries++;
             int presentRandomNumb = Engine.getRandomNumberToTen();
-            String userAnswer = Engine.getUserStringAnswer(StaticVariables.QUESTION + presentRandomNumb + "\nYour answer: ");
-            correctAnswer = getCorrectAnswer(presentRandomNumb);
-            if ((userAnswer.equalsIgnoreCase("yes") && isEven(presentRandomNumb))
-                    || (userAnswer.equalsIgnoreCase("no") && !isEven(presentRandomNumb))) {
-                System.out.println("Correct!");
-                counterCorrectAnswer++;
-            } else {
-                System.out.println("'" + userAnswer + "'" + " is wrong answer ;(. Correct answer was '" +
-                        correctAnswer + "'.\nLet's try again, " + App.USER_NAME + "!");
-                App.main(null);
-            }
-        }
+            System.out.println(StaticVariables.QUESTION + presentRandomNumb);
+            System.out.print("Your answer: " + getFinalString(getUserAnswer(),getCorrectAnswer(presentRandomNumb)));
+        } while (tries != 3);
 
         if (counterCorrectAnswer >= StaticVariables.TRIES) {
             System.out.println("Congratulations, " + App.USER_NAME + "!");
@@ -44,20 +41,32 @@ public class EvenGame {
         return correctAnswer;
     }
 
-    private static void checkRightAnswer(int presentRandomNumb, String userAnswer) {
-        if (userAnswer.equalsIgnoreCase("yes") && isEven(presentRandomNumb) ||
-                userAnswer.equalsIgnoreCase("no") && !isEven(presentRandomNumb)) {
-            System.out.println("Correct!");
-        } else {
-            System.out.println("you made Mistake");
-        }
-    }
-
     public static boolean isCorrect(String correctAnswer, String userAnswer) {
-        return correctAnswer.equals(userAnswer);
+        return correctAnswer.equalsIgnoreCase(userAnswer);
+    }
+    public static String getFinalString(String answer, String corAns) {
+        if (answer.equalsIgnoreCase(corAns)) {
+            return  "Correct!\n";
+        } else {
+            return  "\033[1A" + " is wrong answer ;(. Correct answer was '" +
+                    corAns + "'.\nLet's try again, " + App.USER_NAME + "!";
+        }
     }
 
     public static boolean isEven(int randomNumb) {
         return randomNumb % 2 == 0;
+    }
+    public static String getUserAnswer(){
+        String answer = SCAN.next();
+        return answer;
+    }
+    public static String getResult(String userAnswer, String correctAnswer){
+        String result;
+        if (userAnswer.equalsIgnoreCase(correctAnswer)) {
+            return result = "\033[1A" + "Correct!\n";
+        } else {
+            return result = "\033[1A" + " is wrong answer ;(. Correct answer was '" +
+                    correctAnswer + "'.\nLet's try again, " + App.USER_NAME + "!";
+        }
     }
 }
