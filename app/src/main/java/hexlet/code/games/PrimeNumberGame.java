c0278fb649f55bee32af7e0c6cc0dde08dd56b6b
DARYA SHCHEGLOVA <DaryaSHCH@github.com>
2022-11-09 17:58:00 +0400

    update app/src/main/java/hexlet/code/games/PrimeNumberGame.java

--- app/src/main/java/hexlet/code/games/PrimeNumberGame.java
+++ app/src/main/java/hexlet/code/games/PrimeNumberGame.java
@@ -32,12 +32,12 @@ public class PrimeNumberGame {
                     correctAnswer = "yes";
                 }
                 System.out.println("'" + userAnswer + "'" + " is wrong answer ;(. Correct answer was '"
-                        + correctAnswer + "'.\nLet's try again, " + App.USER_NAME + "!");
+                        + correctAnswer + "'.\nLet's try again, " + App.userName + "!");
                 break;
             }
         }
         if (counterRightAnswer >= StaticVariables.TRIES) {
-            System.out.println("Congratulations, " + App.USER_NAME + "!");
+            System.out.println("Congratulations, " + App.userName + "!");
         }
     }
 
@@ -46,19 +46,28 @@ public class PrimeNumberGame {
         final int caseForThree = 3;
         final int firstDeterminantCycle = 5;
         final int secondDeterminantCycle = 6;
-        {
-            if (number <= caseForOne)
-                return false;
-            if (number <= caseForThree)
-                return true;
-            if (number % 2 == 0 || number % caseForThree == 0)
-                return false;
-            for (int i = firstDeterminantCycle; i * i <= number; i = i + secondDeterminantCycle)
-                if (number % i == 0 || number % (i + 2) == 0)
-                    return false;
+
+        if (number <= caseForOne) {
+            return false;
+        }
+
+        if (number <= caseForThree) {
             return true;
         }
+
+        if (number % 2 == 0 || number % caseForThree == 0) {
+            return false;
+        }
+
+        for (int i = firstDeterminantCycle; i * i <= number; i = i + secondDeterminantCycle) {
+            if (number % i == 0 || number % (i + 2) == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
+
     private static String getCorrectAnswer(int randomNumb) {
         String correctAnswer;
         if (isPrime(randomNumb)) {
