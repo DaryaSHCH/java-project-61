fde93bac276775a34583389d82a3b6281912d55b
DARYA SHCHEGLOVA <DaryaSHCH@github.com>
2022-11-09 17:11:43 +0400

    update app/src/main/java/hexlet/code/StaticVariables.java

--- app/src/main/java/hexlet/code/StaticVariables.java
+++ app/src/main/java/hexlet/code/StaticVariables.java
@@ -1,18 +1,28 @@
 package hexlet.code;
 
 public class StaticVariables {
-    final static int USER_CHOICE_FOR_GREETING = 1;
-    final static int USER_CHOICE_FOR_START_EVEN_GAME = 2;
-    final static int USER_CHOICE_FOR_EXIT = 0;
 
-    final static int USER_CHOICE_FOR_CALCULATOR = 3;
-    final static int USER_CHOICE_FOR_GCD = 4;
-    final static int USER_CHOICE_FOR_PROGRESSION = 5;
-    final static int USER_CHOICE_FOR_PRIME = 6;
+    public static final  String USER_CHOICE = """
+            Please enter the game number and press Enter\s
+            1 - Greet
+            2 - Even\s
+            3 - Calc\s
+            4 - GCD\s
+            5 - Progression\s
+            6 - Prime\s
+            0 - Exit""";
+    static final int USER_CHOICE_FOR_GREETING = 1;
+    static final int USER_CHOICE_FOR_START_EVEN_GAME = 2;
+    static final int USER_CHOICE_FOR_EXIT = 0;
+
+    static final int USER_CHOICE_FOR_CALCULATOR = 3;
+    static final int USER_CHOICE_FOR_GCD = 4;
+    static final int USER_CHOICE_FOR_PROGRESSION = 5;
+    static final int USER_CHOICE_FOR_PRIME = 6;
 
     public final static String QUESTION = "Question: ";
 
-    public final static String ANSWER = "Your answer: ";
+    public static final String ANSWER = "Your answer: ";
 
-    public final static int TRIES = 3;
+    public static final int TRIES = 3;
 }
