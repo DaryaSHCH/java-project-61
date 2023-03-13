26d301b5aaf1985265622b13ee2f3d040bbff6ef
DARYA SHCHEGLOVA <DaryaSHCH@github.com>
2023-03-13 21:25:41 +0400

    refactoring

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,10 @@
 package hexlet.code;
 
-import hexlet.code.games.*;
+import hexlet.code.games.CalculatorGame;
+import hexlet.code.games.EvenGame;
+import hexlet.code.games.GreatestCommonDivisorGame;
+import hexlet.code.games.PrimeNumberGame;
+import hexlet.code.games.ArithmeticProgression;
 
 import static hexlet.code.Cli.greetUser;
 import static hexlet.code.Engine.closeSCAN;
